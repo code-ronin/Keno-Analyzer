5ec510b46589f7b0610fed0e0aa9362e5330ed54
Raymond Li <li.raymond9@gmail.com>
2016-09-12 16:57:36 -0400

    Update Main; Added more data
    More stuff

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import getKenoData
 import analyzeKenoData
 from datetime import date
+from calendar import monthrange
 
+'''
+Various triggers for certain things:
+Trigger 1 = Gather data from single month
+Trigger 2 = Keno Analysis
+'''
+trigger = 1
+#Variables for large grab
+year = 2015
+month = "01"
+if trigger == 1:
+    #Calculate days in month that year
+    days = monthrange(year,int(month))[1]
+    for x in range(1, days+1):
+        day = str(x)
+        pastURL = getKenoData.getPastKenoURL(year, month, day)
+        htmlFile = getKenoData.getKenoNums(pastURL)
+        getKenoData.parseFile(htmlFile)
+if trigger == 2:
+    print("Test")
 #Step 1: Get Data from MA Keno site
 #Get Yesterday's URL
 '''
@@ -11,8 +31,8 @@ kenoURL = getKenoData.getYesterdayURL(today)
 htmlFile = getKenoData.getKenoNums(kenoURL)
 #Parse the HTML saved in the text file
 kenoFile = getKenoData.parseFile(htmlFile)
-'''
 
+'''
 #Below is code to grab URL from the past
 #will use later for mass data collection
 #yesteryear = kenoNumParser.getPastKeno(2014, "02", "14")
@@ -31,6 +51,6 @@ kenoFile = getKenoData.parseFile(htmlFile)
 
 #Step 3: Run overall analysis of data
 #Numbers most currently represented
-analyzeKenoData.scanAll()
+#analyzeKenoData.scanAll()
 
 #Numbers most currently under-represented
